427f4908a7abc0ea241311ad5db6b74d36c3e074
Dmitrii Larionov <mikrolux@gmail.com>
2023-12-10 18:40:34 +0700

    form+db working

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 
 export default function Menu() {
 	return (
@@ -6,15 +7,16 @@ export default function Menu() {
 				<a className="italic text-zinc-400" href="/">Logo</a>
 			</div>
 			<div className='grid md:grid-cols-4 md:place-items-center mt-4 md:mt-0 mb-4 md:mb-0'>
-				<a href="/lp" className='text-sm py-1'>Product</a>
+				<Link className='text-sm py-1' href="/lp">Product</Link>
 				<a href="" className='text-sm py-1'>Docs</a>
 				<a href="" className='text-sm py-1'>Pricing</a>
 				<a href="" className='text-sm py-1'>Contact</a>
 			</div>
 			<div className='grid md:grid-cols-2 md:items-center md:place-items-end'>
-				<a href="/signup" className='text-sm py-2'>Signup</a>
+				<Link className='text-sm py-2' href="/signup">Sign Up</Link>
 				<button className='text-md w-fit mt-2 text-white mb-2 rounded-full bg-sky-600 px-4 py-1.5'>Try for free</button>
 			</div>
 		</div>
 	)
 }
+
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,56 +1,56 @@
 import Image from 'next/image'
-import { PrismaClient } from "@prisma/client"
 import Menu from '../Menu'
 import Footer from '../Footer'
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
-// const [errorStat, setErrorStat] = useState('');
-
 
 export default function Home() {
-	const addUser = async(formData: FormData) => {
+
+	const formAddUser = async (formData: FormData) => {
 		"use server"
 
 		const name = formData.get('name')
 		const email = formData.get('email')
-		// console.log(name)
 
-		// const checkEmail = await prisma.user.findMany ({
-		// 	where: { email: email }
-		// })
-		// if (checkEmail)
-		// {
-		// 	errorStat = "this email is already used"
-  		// 	return
-		// }
-		const user = await prisma.user.create({
-			data: {
-			  name: name,
-			  email: email,
-			},
-		});
-		console.log(user)
+
+		try {
+			await prisma.user.create({
+				data: {
+				name: name as string,
+				email: email as string,
+				}
+			});
+
+			console.log("User added successfully!");
+
+			} catch (error) {
+				console.error("Error adding user:", error);
+		}
 	}
 
-  return (
-    <main>
-		<div className="px-6 py-12 max-w-screen-xl mx-auto">
+	return (
+		<main>
+			<div className="px-6 py-12 max-w-screen-xl mx-auto">
+				
+				<Menu />
 			
-			<Menu />		
-		
-			<div className="mb-12 w-full grid gap-4">
-				<form className="max-w-xs shadow p-8 mx-auto" action={addUser} method="POST" encType="multipart/form-data">
-						<label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Username</label>
+				<div className="mb-12 w-full grid gap-4">
+					<div className="mx-auto text-center">
+						<div className="text-3xl font-semibold mb-4 mt-12">Sign Up</div>
+					</div>
+					<form action={formAddUser} className="border rounded border-gray-300 overflow-hidden text-sm max-w-xs p-8 mx-auto">
+						<label htmlFor="name" className="block text-gray-700 text-sm font-semibold mb-2">Username</label>
 						<input className="max-w-full rounded border-2 py-2 px-4 mb-4" type="text" name="name" />
-						<label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+						<label htmlFor="email" className="block text-gray-700 text-sm font-semibold mb-2">Email</label>
 						<input className="max-w-full rounded border-2 py-2 px-4 mb-4" type="email" name="email" />
-					<button className="py-2 px-4 bg-sky-600  text-white rounded" type="submit">Submit</button>
-				</form>
-				{/* <div id="formError" className="">{errorStat}</div> */}
+						<button className="mr-8 bg-sky-600 px-6 py-2 rounded text-white border-2 border-transparent font-light" type="submit">Sign Up</button>
+					</form>
+					{/* <div id="formError" className="">{errorStat}</div> */}
+				</div>
+		
 			</div>
-	
-		</div>
-		<Footer />
-    </main>
-  )
+			<Footer />
+		</main>
+	)
 }
